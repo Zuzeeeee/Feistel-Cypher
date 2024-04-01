7cc76626a50903b925da9b7cb95c4994f47fda6f
Zuzeeeee <josealves7jk@gmail.com>
2024-04-01 15:25:26 -0300

    Co-authored-by: Fernando Marques <fernando.silva@ccc.ufcg.edu.br>

--- TCPClient.py
+++ TCPClient.py
@@ -7,9 +7,29 @@ def block_selector(msg):
 
 def encrypt_message(msg, key):
     half = len(msg)//2
-    left, right = msg[:half], msg[half:]
-    new_right = "".join([right[int(k) - 1] for k in str(key)]) ^ left
-    print(new_right)
+    for i in range(4):
+        left, right = msg[:half], msg[half:]
+        msg = xor(left,right)
+        print(msg)
+    return msg
+
+
+def shuffle(right, key):
+    #fazer aqui
+    return
+
+def xor(left,right):
+    right = list(right)
+    print(right)
+    right = list(map(ord, right))
+    left = list(left)
+    left = list(map(ord, left))
+    for i in range(0,len(left)):
+        right[i] = right[i] ^ left[i]
+    msg = right + left
+    msg = "".join(list(map(chr, msg)))
+    return msg
+    
     
 
 serverName = '127.0.0.1'
